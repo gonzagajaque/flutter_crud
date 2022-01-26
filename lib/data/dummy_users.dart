709da8b64f02f9fbb67f28f4042305762191917d
jaquelinegonzaga_estuda <jaqueline.gonzaga@estuda.com>
2022-01-25 21:44:39 -0400

    feat: novas telas

--- lib/data/dummy_users.dart
+++ lib/data/dummy_users.dart
@@ -5,28 +5,24 @@ const DUMMY_USERS = {
     id: '1',
     name: 'Maria',
     email: 'maria@alunos.com.br',
-    avatarUrl:
-        'https://cdn.pixabay.com/photo/2020/04/15/17/32/medical-5047582_1280.png',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2016/04/26/07/57/woman-1353825_960_720.png',
   ),
   '2': User(
     id: '2',
     name: 'Rafael',
     email: 'rafael@alunos.com.br',
-    avatarUrl:
-        'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_1280.png',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2018/08/28/12/41/avatar-3637425_960_720.png',
   ),
   '3': User(
     id: '3',
     name: 'Fernanda',
     email: 'fernanda@zymail.com.br',
-    avatarUrl:
-        'https://cdn.pixabay.com/photo/2016/03/31/20/27/avatar-1295773_1280.png',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2013/07/12/14/07/student-147783_960_720.png',
   ),
   '4': User(
     id: '4',
     name: 'Guilherme',
     email: 'guigui@abcmail.com',
-    avatarUrl:
-        'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_1280.png',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_960_720.png',
   )
 };
